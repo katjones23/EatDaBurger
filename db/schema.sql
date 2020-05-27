9c3e6e5aa73f4509f8a7eeb5cf5e1fb118c59c23
Katherine Jones <kejone1414@gmail.com>
2020-05-26 21:35:36 -0400

    feat: added initial files and instructions

---
+++ db/schema.sql
@@ -0,0 +1,14 @@
+-- -- Create the burgers_db.
+-- -- Switch to or use the burgers_db.
+-- -- Create a burgers table with these fields:
+
+
+-- -- id: an auto incrementing int that serves as the primary key.
+
+-- -- burger_name: a string.
+
+-- -- devoured: a boolean.
+-- Start MySQL command line tool and login: mysql -u root -p.
+
+
+-- With the mysql> command line tool running, enter the command source schema.sql. This will run your schema file and all of the queries in it -- in other words, you'll be creating your database.
---
+++ db/seeds.sql
@@ -0,0 +1,2 @@
+-- -- write insert queries to populate the burgers table with at least three entries.
+-- Now insert the entries you defined in seeds.sql by running the file: source seeds.sql.
