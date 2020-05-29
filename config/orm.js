ee760a29a223115674ad588fa1f59c1d19efe535
Katherine Jones <kejone1414@gmail.com>
2020-05-29 18:59:48 -0400

    fix: fixed issue with data not being returned

--- config/orm.js
+++ config/orm.js
@@ -1,27 +1,30 @@
 const connection = require('./connection');
 
 const orm = {
-  selectAll: function (table) {
+  selectAll: function (table, cb) {
     let queryString = "SELECT * FROM ??";
 
     connection.query(queryString, [table], function (err, res) {
       if (err) throw err;
+      cb(res)
     });
   },
 
-  insertOne: function (table, column, value) {
+  insertOne: function (table, column, value, cb) {
     let queryString = "INSERT INTO ?? (??) VALUES (?)";
 
     connection.query(queryString, [table, column, value], function (err, result) {
       if (err) throw err;
+      cb(res)
     });
   },
 
-  updateOne: function (table, column, value, id) {
+  updateOne: function (table, column, value, id, cb) {
     let queryString = "UPDATE ?? SET ?? = ? WHERE id = ?";
 
     connection.query(queryString, [table, column, value, id], function (err, result) {
       if (err) throw err;
+      cb(res)
     }
     );
   }
