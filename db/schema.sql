e16990e838cd08b36db5e1101dc2bcc4ed7bc92f
Katherine Jones <kejone1414@gmail.com>
2020-05-29 17:39:47 -0400

    feat: added server.js, frontend.js, modified other files

--- db/schema.sql
+++ db/schema.sql
@@ -6,9 +6,7 @@ USE burgers_db;
 CREATE TABLE `burgers`
 (
   id INT AUTO_INCREMENT NOT NULL,
-  burger_name VARCHAR
-(300) NOT NULL,
-  devoured BOOLEAN,
-  PRIMARY KEY
-(id)
+  burger_name VARCHAR (300) NOT NULL,
+  devoured BOOLEAN DEFAULT false,
+  PRIMARY KEY (id)
 );
